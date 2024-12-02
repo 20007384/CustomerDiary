3e376553f4451b6d9fe92f04f5ef90098253bf2f
20007384 <20007384@mydbs.ie>
2024-12-02 15:54:42 +0530

    database initialized

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express=require('express');
 const dotenv=require('dotenv');
 const mysql=require('mysql2');
 
+const connection=mysql.createConnection({
+    host:"127.0.0.1",
+    user:"root",
+    password:"password",
+    database:"CRUDNODEJSPAULSQL"
+});
+
+connection.connect(e=>{
+    if(e)
+    {
+        console.log("database not connected!");
+        return;
+    }
+    console.log("Database connected!");
+})
 
 const app=express();
 app.use(express.json());
