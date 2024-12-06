3db68ba79768ab2121daefb209dee7c5491bfdc4
20007384 <20007384@mydbs.ie>
2024-12-06 12:06:10 +0530

    styling

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view-engine',ejs);
 app.use('/static', express.static(path.join(__dirname,'static')));
 app.use(express.urlencoded({extended:true}));
 
-app.use(express.json());
+app.use(express.json({}));
 
 
 
